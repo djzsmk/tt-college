2e434c084f3cfb3bd19647e6092616185773e769
Michael_J_T <lightbar200@gmail.com>
2026-06-26 13:12:33 -0500

    added exports for functionality

---
+++ src/pages/Adventure.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Adventure () {
+    return (
+        <h1>Adventure</h1>
+    )
+};
+
+export default Adventure
---
+++ src/pages/CharacterCreator.jsx
@@ -0,0 +1,7 @@
+function CharacterCreator () {
+    return (
+        <h2>Character creator</h2>
+    )
+}
+
+export default CharacterCreator
